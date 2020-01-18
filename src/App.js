8530c7bc9d0b2569b02cc42eaee71273a10c2886
Steve Mullins <sgmullins@gmail.com>
2020-01-18 08:52:44 -0700

    Create boilerplate

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import { Router, Link } from "@reach/router";
+
+const App = () => {
+  return <div></div>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
