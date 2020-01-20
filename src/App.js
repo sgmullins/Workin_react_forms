c31975393c2e86f71374afb492133fd5257846cd
Steve Mullins <sgmullins@gmail.com>
2020-01-20 10:34:50 -0700

    Add router and routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Router, Link } from "@reach/router";
+
 import LoginForm from "./LoginForm";
 import RegisterForm from "./RegisterForm";
 
 const App = () => {
   return (
     <div>
-      <LoginForm />
-      <RegisterForm />
+      <nav>
+        <Link to="/">Register</Link>
+        <Link to="/login">Login</Link>
+      </nav>
+      <Router>
+        <LoginForm path="/login" />
+        <RegisterForm path="/" />
+      </Router>
     </div>
   );
 };
